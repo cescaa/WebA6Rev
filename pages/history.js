5e30cfe8be634ba8adeb29e9d964154f8a7dc884
Cesca <32521038+cescaa@users.noreply.github.com>
2024-11-29 21:27:30 -0500

    Update history.js

--- pages/history.js
+++ pages/history.js
@@ -1,4 +1,4 @@
-import { useAtom } from "jotai";
+/*import { useAtom } from "jotai";
 import { searchHistoryAtom } from "../store";
 import { useRouter } from "next/router";
 import { ListGroup, Card, Button } from "react-bootstrap";
@@ -33,6 +33,73 @@ export default function History() {
       let updatedHistory = [...current];
       updatedHistory.splice(index, 1);
       return updatedHistory;
+    });
+  };
+
+  return (
+    <div>
+      <h1>Search History</h1>
+      {parsedHistory.length === 0 ? (
+        <Card>
+          <Card.Body>Nothing Here. Try searching for some artwork.</Card.Body>
+        </Card>
+      ) : (
+        <ListGroup>
+          {parsedHistory.map((historyItem, index) => (
+            <ListGroup.Item
+              key={index}
+              className={styles.historyListItem}
+              onClick={(e) => historyClicked(e, index)}
+            >
+              {Object.keys(historyItem).map((key) => (
+                <span key={key}>
+                  {key}: <strong>{historyItem[key]}</strong>&nbsp;
+                </span>
+              ))}
+              <Button
+                className="float-end"
+                variant="danger"
+                size="sm"
+                onClick={(e) => removeHistoryClicked(e, index)}
+              >
+                &times;
+              </Button>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      )}
+    </div>
+  );
+} */
+
+import { useAtom } from "jotai";
+import { searchHistoryAtom } from "../store";
+import { useRouter } from "next/router";
+import { ListGroup, Card, Button } from "react-bootstrap";
+import styles from "@/styles/History.module.css"; // Import CSS module
+
+export default function History() {
+  const [searchHistory, setSearchHistory] = useAtom(searchHistoryAtom);
+  const router = useRouter();
+
+  let parsedHistory = [];
+  searchHistory.forEach((h) => {
+    let params = new URLSearchParams(h);
+    let entries = params.entries();
+    parsedHistory.push(Object.fromEntries(entries));
+  });
+
+  const historyClicked = (e, index) => {
+    e.stopPropagation();
+    router.push(`/artwork?${searchHistory[index]}`);
+  };
+
+  const removeHistoryClicked = (e, index) => {
+    e.stopPropagation();
+    setSearchHistory((current) => {
+      let x = [...current];
+      x.splice(index, 1);
+      return x;
     });
   };
 
